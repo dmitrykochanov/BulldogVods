63cc0a4ffb21719093d042dbde3397b2ab9e2217
Dmitry Kochanov <dmitrykochanov1@gmail.com>
2022-05-02 21:52:21 +0400

    di modules refactoring

---
+++ app/src/main/kotlin/com/dmko/bulldogvods/di/modules/CommonModule.kt
@@ -0,0 +1,35 @@
+package com.dmko.bulldogvods.di.modules
+
+import com.dmko.bulldogvods.app.common.imageloader.CoilImageLoader
+import com.dmko.bulldogvods.app.common.imageloader.ImageLoader
+import com.dmko.bulldogvods.app.common.schedulers.Schedulers
+import com.dmko.bulldogvods.app.common.schedulers.SchedulersImpl
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import org.greenrobot.eventbus.EventBus
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface CommonModule {
+
+    @Binds
+    @Singleton
+    fun provideImageLoader(coilImageLoader: CoilImageLoader): ImageLoader
+
+    @Binds
+    @Singleton
+    fun provideSchedulers(schedulersImpl: SchedulersImpl): Schedulers
+
+    companion object {
+
+        @Provides
+        @Singleton
+        fun provideEventBus(): EventBus {
+            return EventBus.getDefault()
+        }
+    }
+}
