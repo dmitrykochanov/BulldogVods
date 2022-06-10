7fbb3406c438f951381530d31c199e8967dcba34
Dmitry Kochanov <dmitrykochanov1@gmail.com>
2022-06-10 20:17:00 +0400

    Dynamic colors

--- app/src/main/kotlin/com/dmko/bulldogvods/app/App.kt
+++ app/src/main/kotlin/com/dmko/bulldogvods/app/App.kt
@@ -1,7 +1,13 @@
 package com.dmko.bulldogvods.app
 
 import android.app.Application
+import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+
+    override fun onCreate() {
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}
