1852193344c27e95d50aa2c3e59fd0eed8413e03
Dmitry Kochanov <dmitrykochanov1@gmail.com>
2022-06-12 17:05:07 +0400

    Exo player 30 seconds connection and read delay

--- app/src/main/kotlin/com/dmko/bulldogvods/features/vods/presentation/player/ExoPlayerFactory.kt
+++ app/src/main/kotlin/com/dmko/bulldogvods/features/vods/presentation/player/ExoPlayerFactory.kt
@@ -2,6 +2,9 @@ package com.dmko.bulldogvods.features.vods.presentation.player
 
 import android.content.Context
 import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
+import com.google.android.exoplayer2.source.DefaultMediaSourceFactory
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
@@ -10,7 +13,11 @@ class ExoPlayerFactory @Inject constructor(
 ) {
 
     fun createExoPlayer(): ExoPlayer {
+        val dataSourceFactory = DefaultHttpDataSource.Factory()
+            .setConnectTimeoutMs(CONNECT_TIMEOUT_MILLIS)
+            .setReadTimeoutMs(READ_TIMEOUT_MILLIS)
         val exoPlayer = ExoPlayer.Builder(context)
+            .setMediaSourceFactory(DefaultMediaSourceFactory(dataSourceFactory, DefaultExtractorsFactory()))
             .setSeekBackIncrementMs(SEEK_INCREMENT_MILLIS)
             .setSeekForwardIncrementMs(SEEK_INCREMENT_MILLIS)
             .build()
@@ -22,5 +29,7 @@ class ExoPlayerFactory @Inject constructor(
     private companion object {
 
         private const val SEEK_INCREMENT_MILLIS = 10_000L
+        private const val CONNECT_TIMEOUT_MILLIS = 30_000
+        private const val READ_TIMEOUT_MILLIS = 30_000
     }
 }
