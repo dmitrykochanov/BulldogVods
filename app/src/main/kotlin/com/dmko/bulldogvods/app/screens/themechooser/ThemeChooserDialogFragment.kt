d8fc6e5ec066888e9ad75ca52431304001606aa0
Dmitry Kochanov <dmitrykochanov1@gmail.com>
2022-06-13 18:31:18 +0400

    Show app version

--- app/src/main/kotlin/com/dmko/bulldogvods/app/screens/themechooser/ThemeChooserDialogFragment.kt
+++ app/src/main/kotlin/com/dmko/bulldogvods/app/screens/themechooser/ThemeChooserDialogFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.appcompat.app.AppCompatDialogFragment
 import androidx.core.view.isInvisible
 import androidx.fragment.app.viewModels
+import com.dmko.bulldogvods.BuildConfig
 import com.dmko.bulldogvods.R
 import com.dmko.bulldogvods.app.common.binding.viewBinding
 import com.dmko.bulldogvods.databinding.DialogFragmentThemeChooserBinding
@@ -21,6 +22,7 @@ class ThemeChooserDialogFragment : AppCompatDialogFragment(R.layout.dialog_fragm
         binding.layoutThemeDark.setOnClickListener { viewModel.onThemeClicked(Theme.DARK) }
         binding.layoutThemeLight.setOnClickListener { viewModel.onThemeClicked(Theme.LIGHT) }
         binding.layoutThemeSystemDefault.setOnClickListener { viewModel.onThemeClicked(Theme.SYSTEM_DEFAULT) }
+        binding.appVersionText.text = getString(R.string.dialog_theme_chooser_app_version, BuildConfig.VERSION_NAME)
 
         viewModel.selectedThemeLiveData.observe(viewLifecycleOwner, ::showSelectedTheme)
     }
