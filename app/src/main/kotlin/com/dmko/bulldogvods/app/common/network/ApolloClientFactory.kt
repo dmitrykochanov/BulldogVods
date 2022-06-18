4af4eb1a2591091e389ed7679451dc8389763bad
Dmitry Kochanov <dmitrykochanov1@gmail.com>
2022-06-18 16:36:49 +0400

    Removed database cache

--- app/src/main/kotlin/com/dmko/bulldogvods/app/common/network/ApolloClientFactory.kt
+++ app/src/main/kotlin/com/dmko/bulldogvods/app/common/network/ApolloClientFactory.kt
@@ -3,7 +3,6 @@ package com.dmko.bulldogvods.app.common.network
 import com.apollographql.apollo3.ApolloClient
 import com.apollographql.apollo3.cache.normalized.api.MemoryCacheFactory
 import com.apollographql.apollo3.cache.normalized.normalizedCache
-import com.apollographql.apollo3.cache.normalized.sql.SqlNormalizedCacheFactory
 import com.apollographql.apollo3.network.okHttpClient
 import com.dmko.bulldogvods.BuildConfig
 import okhttp3.OkHttpClient
@@ -23,12 +22,10 @@ class ApolloClientFactory @Inject constructor() {
         val loggingOkHttpClient = OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
             .build()
-        val memoryCacheFactory = MemoryCacheFactory(maxSizeBytes = MEMORY_CACHE_SIZE_BYTES)
-        val databaseCacheFactory = SqlNormalizedCacheFactory(DATABASE_CACHE_NAME)
         return ApolloClient.Builder()
             .serverUrl(BASE_URL)
             .okHttpClient(loggingOkHttpClient)
-            .normalizedCache(memoryCacheFactory.chain(databaseCacheFactory), writeToCacheAsynchronously = true)
+            .normalizedCache(MemoryCacheFactory(maxSizeBytes = MEMORY_CACHE_SIZE_BYTES))
             .build()
     }
 
@@ -36,6 +33,5 @@ class ApolloClientFactory @Inject constructor() {
 
         private const val BASE_URL = "https://vods.admiralbulldog.live/api/gql"
         private const val MEMORY_CACHE_SIZE_BYTES = 10 * 1024 * 1024
-        private const val DATABASE_CACHE_NAME = "bulldog_vods_apollo_cache.db"
     }
 }
