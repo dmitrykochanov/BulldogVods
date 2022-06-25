4c02787d46cd3c5f2d312581a783e1b74a95ad6f
Dmitry Kochanov <dmitrykochanov1@gmail.com>
2022-06-25 16:40:55 +0400

    Chat messages fetch policy

--- app/src/main/kotlin/com/dmko/bulldogvods/features/chat/data/network/datasource/ApolloNetworkChatDataSource.kt
+++ app/src/main/kotlin/com/dmko/bulldogvods/features/chat/data/network/datasource/ApolloNetworkChatDataSource.kt
@@ -2,7 +2,9 @@ package com.dmko.bulldogvods.features.chat.data.network.datasource
 
 import apollo.ChatMessagesQuery
 import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.cache.normalized.FetchPolicy
 import com.apollographql.apollo3.cache.normalized.doNotStore
+import com.apollographql.apollo3.cache.normalized.fetchPolicy
 import com.apollographql.apollo3.rx3.rxSingle
 import com.dmko.bulldogvods.features.chat.data.network.mapping.MessageSchemaToChatMessageMapper
 import com.dmko.bulldogvods.features.chat.data.network.mapping.TimestampToDateStringMapper
@@ -24,6 +26,7 @@ class ApolloNetworkChatDataSource @Inject constructor(
         )
         return apolloClient.query(query)
             .doNotStore(true)
+            .fetchPolicy(FetchPolicy.NetworkOnly)
             .rxSingle()
             .map { apolloResponse -> requireNotNull(apolloResponse.data?.messages) }
             .map { chatMessages -> chatMessages.map(messageSchemaToChatMessageMapper::map) }
