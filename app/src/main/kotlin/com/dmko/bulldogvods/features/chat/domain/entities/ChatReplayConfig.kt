afb5507a795c9f15d6c5649c16c8c1840cb69866
Dmitry Kochanov <dmitrykochanov1@gmail.com>
2022-06-25 21:12:52 +0400

    Increase chat history size and initial preload offset

--- app/src/main/kotlin/com/dmko/bulldogvods/features/chat/domain/entities/ChatReplayConfig.kt
+++ app/src/main/kotlin/com/dmko/bulldogvods/features/chat/domain/entities/ChatReplayConfig.kt
@@ -1,12 +1,13 @@
 package com.dmko.bulldogvods.features.chat.domain.entities
 
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 
 data class ChatReplayConfig(
     val pageSize: Duration = 20.seconds,
-    val historySize: Duration = 60.seconds,
-    val initialPreloadOffset: Duration = 10.seconds,
+    val historySize: Duration = 3.minutes,
+    val initialPreloadOffset: Duration = 20.seconds,
     val prefetchDelay: Duration = 5.seconds,
     val playbackPositionOffset: Duration = 6.5.seconds
 )
