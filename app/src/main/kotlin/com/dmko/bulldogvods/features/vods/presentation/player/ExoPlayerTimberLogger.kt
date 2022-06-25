b8bfbfa838ee98b9007857b8855e50c92a7d17d1
Dmitry Kochanov <dmitrykochanov1@gmail.com>
2022-06-25 21:30:35 +0400

    Using empty exo player event logger constructor

--- app/src/main/kotlin/com/dmko/bulldogvods/features/vods/presentation/player/ExoPlayerTimberLogger.kt
+++ app/src/main/kotlin/com/dmko/bulldogvods/features/vods/presentation/player/ExoPlayerTimberLogger.kt
@@ -3,7 +3,7 @@ package com.dmko.bulldogvods.features.vods.presentation.player
 import com.google.android.exoplayer2.util.EventLogger
 import timber.log.Timber
 
-class ExoPlayerTimberLogger : EventLogger(null) {
+class ExoPlayerTimberLogger : EventLogger() {
 
     override fun logd(message: String) {
         Timber.d(message)
